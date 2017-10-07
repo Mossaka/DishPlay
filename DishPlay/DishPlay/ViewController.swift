f6895956d982d1dbe2443a66ceac22feb6599490
DYBCyrus <yad025@ucsd.edu>
2017-10-07 07:16:08 -0700

    revise AR view

--- DishPlay/DishPlay/ViewController.swift
+++ DishPlay/DishPlay/ViewController.swift
@@ -18,7 +18,11 @@ class ViewController: UIViewController, ARSCNViewDelegate, G8TesseractDelegate{
 
     
     @IBOutlet var sceneView: ARSCNView!
-    
+	
+	var allNodes: [SCNNode] = []
+	var cardNumbers = 0
+	var touch: UITouch!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -29,7 +33,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, G8TesseractDelegate{
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -38,6 +42,163 @@ class ViewController: UIViewController, ARSCNViewDelegate, G8TesseractDelegate{
         view.addGestureRecognizer(tapGesture)
 
     }
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		touch = touches.first
+		let nodeResults = sceneView.hitTest(touch.location(in: sceneView), options: nil)
+		for result in nodeResults {
+			if allNodes.contains(result.node) {
+				result.node.runAction(SCNAction.wait(duration: 0.5), completionHandler: {
+					result.node.parent?.removeFromParentNode()
+					self.allNodes.remove(at: self.allNodes.index(of: result.node)!)
+					self.cardNumbers = 0
+				})
+			}
+			
+			for node in allNodes {
+				if result.node == node.parent {
+					return
+				}
+			}
+		}
+//		guard let touch = touches.first else { return }
+//		var hitOnCard = false
+//		let nodeResults = sceneView.hitTest(touch.location(in: sceneView), options: nil)
+//		for result in nodeResults {
+//			if allNodes.contains(result.node) {
+//				hitOnCard = true
+//				result.node.runAction(SCNAction.wait(duration: 0.5), completionHandler: {
+//					result.node.parent?.removeFromParentNode()
+//					self.allNodes.remove(at: self.allNodes.index(of: result.node)!)
+//					self.cardNumbers = 0
+//				})
+//			}
+//			for node in allNodes {
+//				if result.node == node.parent {
+//					return
+//				}
+//			}
+//		}
+//		if !hitOnCard && image != nil && cardNumbers == 0 {
+//			cardNumbers = 1
+//			let results = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+//			guard let hitFeature = results.last else { return }
+//			let hitTransform = SCNMatrix4(hitFeature.worldTransform)
+//			let hitPosition = SCNVector3Make(hitTransform.m41,
+//											 hitTransform.m42,
+//											 hitTransform.m43)
+//
+//			let billboardConstraint = SCNBillboardConstraint()
+//			billboardConstraint.freeAxes = SCNBillboardAxis.Y
+//
+//			let backNode = SCNNode()
+//			// backNode.scale = SCNVector3(x: 0.5, y: 0.5, z: 0.5)
+//			let plaque = SCNBox(width: 0.14, height: 0.1, length: 0.01, chamferRadius: 0.005)
+//			plaque.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.6)
+//			backNode.geometry = plaque
+//			// backNode.position = hitPosition
+//
+//			//Set up card view
+//			let imageView = UIView(frame: CGRect(x: 0, y: 0, width: 800, height: 600))
+//			imageView.backgroundColor = .clear
+//			imageView.alpha = 1.0
+//
+//			let circleLabel = UILabel(frame: CGRect(x: imageView.frame.width - 144, y: 48, width: 96, height: 96))
+//			circleLabel.layer.cornerRadius = 48
+//			circleLabel.clipsToBounds = true
+//			circleLabel.backgroundColor = .red
+//			imageView.addSubview(circleLabel)
+//
+//			let imageToDisplay = image
+//			//let imageview = UIImageView(image: image)
+//			//imageView.addSubview(imageview)
+//
+//			let infoNode = SCNNode()
+//			//infoNode.scale = SCNVector3(x: 0.5, y: 0.5, z: 0.5)
+//			let infoGeometry = SCNPlane(width: 0.13, height: 0.09)
+//			infoGeometry.firstMaterial?.diffuse.contents = imageToDisplay
+//			infoNode.geometry = infoGeometry
+//			infoNode.position = hitPosition
+//			backNode.constraints = [billboardConstraint]
+//			infoNode.constraints = [billboardConstraint]
+//			infoNode.addChildNode(backNode)
+//			sceneView.scene.rootNode.addChildNode(infoNode)
+//			self.allNodes.append(backNode)
+//		}
+	}
+	
+	func touches() {
+		print("create card!")
+		//guard let touch = touchesMade?.first else { return }
+//		var hitOnCard = false
+//		let nodeResults = sceneView.hitTest(touch.location(in: sceneView), options: nil)
+//		for result in nodeResults {
+//			if allNodes.contains(result.node) {
+//				hitOnCard = true
+//				result.node.runAction(SCNAction.wait(duration: 0.5), completionHandler: {
+//					result.node.parent?.removeFromParentNode()
+//					self.allNodes.remove(at: self.allNodes.index(of: result.node)!)
+//					self.cardNumbers = 0
+//				})
+//			}
+//			for node in allNodes {
+//				if result.node == node.parent {
+//					return
+//				}
+//			}
+//		}
+		let results = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+		guard let hitFeature = results.last else { return }
+		let hitTransform = SCNMatrix4(hitFeature.worldTransform)
+		let hitPosition = SCNVector3Make(hitTransform.m41,
+										 hitTransform.m42,
+										 hitTransform.m43)
+		
+		let billboardConstraint = SCNBillboardConstraint()
+		billboardConstraint.freeAxes = SCNBillboardAxis.Y
+		
+		let backNode = SCNNode()
+		let plaque = SCNBox(width: 0.14, height: 0.1, length: 0.01, chamferRadius: 0.005)
+		plaque.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.6)
+		backNode.geometry = plaque
+		
+		//Set up card view
+		let imageView = UIView(frame: CGRect(x: 0, y: 0, width: 800, height: 600))
+		imageView.backgroundColor = .clear
+		imageView.alpha = 1.0
+		
+//		let circleLabel = UILabel(frame: CGRect(x: imageView.frame.width - 144, y: 48, width: 96, height: 96))
+//		circleLabel.layer.cornerRadius = 48
+//		circleLabel.clipsToBounds = true
+//		circleLabel.backgroundColor = .red
+//		imageView.addSubview(circleLabel)
+		
+		let imageToDisplay = image
+//		let imageview = UIImageView(image: imageToDisplay)
+//		imageView.addSubview(imageview)
+		
+		let closeNode = SCNNode()
+		let closeSphere = SCNSphere(radius: 0.012)
+		closeSphere.firstMaterial?.diffuse.contents = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+		closeNode.geometry = closeSphere
+		
+		let infoNode = SCNNode()
+		let infoGeometry = SCNPlane(width: 0.13, height: 0.09)
+		infoNode.rotation = SCNVector4.init(infoNode.rotation.x, infoNode.rotation.y, infoNode.rotation.z, -Float.pi/4)
+		infoGeometry.firstMaterial?.diffuse.contents = imageToDisplay
+		infoNode.geometry = infoGeometry
+		infoNode.position = hitPosition
+		closeNode.position.x = infoNode.position.x + Float(infoGeometry.width / 2)
+		closeNode.position.y = infoNode.position.y + Float(infoGeometry.height / 2)
+		backNode.constraints = [billboardConstraint]
+		infoNode.constraints = [billboardConstraint]
+		closeNode.constraints = [billboardConstraint]
+		infoNode.addChildNode(closeNode)
+		infoNode.addChildNode(backNode)
+		sceneView.scene.rootNode.addChildNode(infoNode)
+		print("Appear!")
+		self.allNodes.append(closeNode)
+	}
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -74,6 +235,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, G8TesseractDelegate{
     var dishName: String = "" {
         willSet {
             // run the image grapping thing
+			if cardNumbers == 0 {
             let headers = [
                 "Ocp-Apim-Subscription-Key": "293bdc29ef6540749d2db90d239bdc0e"
             ]
@@ -98,6 +260,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, G8TesseractDelegate{
                         //for imageValue in value! {
                         if let imageValue = value {
                             if imageValue.count > 0 {
+								self.cardNumbers = 1
                                 self.imageURL = URL(string: (imageValue[0]["contentUrl"] as? String)!)
                                 /*Alamofire.download((imageValue["contentUrl"] as? String)!)
                                  .responseData { response in
@@ -118,11 +281,12 @@ class ViewController: UIViewController, ARSCNViewDelegate, G8TesseractDelegate{
                 }
             }
         }
+		}
     }
     
     var imageURL: URL? {
         didSet {
-            fetchImage()
+			fetchImage()
         }
     }
     
@@ -131,13 +295,23 @@ class ViewController: UIViewController, ARSCNViewDelegate, G8TesseractDelegate{
             let urlContents = try? Data(contentsOf: url)
             if let imageData = urlContents {
                 image = UIImage(data: imageData)
-                print(image)
                 //self.performSegue(withIdentifier: "fromARtoImage", sender: self)
             }
         }
     }
     
     private var image: UIImage?
+	{
+		didSet {
+			if image != nil {
+				touches()
+			} else {
+				cardNumbers = 0
+			}
+			print(image == nil)
+			print(cardNumbers)
+		}
+	}
     
     
     @objc
