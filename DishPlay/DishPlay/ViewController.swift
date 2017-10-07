37c9772c7a1e778dab6c776d3f92828d893d1249
Jiaxiao Zhou <jiz417@ucsd.edu>
2017-10-07 01:21:37 -0700

    works version

--- DishPlay/DishPlay/ViewController.swift
+++ DishPlay/DishPlay/ViewController.swift
@@ -14,9 +14,9 @@ import TesseractOCR
 import SwiftyJSON
 
 
+class ViewController: UIViewController, ARSCNViewDelegate, G8TesseractDelegate{
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -33,6 +33,10 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(gestureReconize:)))
+        view.addGestureRecognizer(tapGesture)
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -67,7 +71,100 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         return node
     }
 */
+    var dishName: String = "" {
+        willSet {
+            // run the image grapping thing
+            let headers = [
+                "Ocp-Apim-Subscription-Key": "293bdc29ef6540749d2db90d239bdc0e"
+            ]
+            var requestParams = [String:AnyObject]()
+            requestParams["q"] = newValue as AnyObject?
+            requestParams["count"] = 10 as AnyObject?
+            requestParams["safeSearch"] = "Strict" as AnyObject?
+            
+            Alamofire.request("https://api.cognitive.microsoft.com/bing/v7.0/images/search",
+              parameters: requestParams,
+              encoding: URLEncoding.default, headers: headers).responseJSON {
+                (response:DataResponse<Any>) in
+                
+                switch(response.result) {
+                case .success(_):
+                    
+                    
+                    if let JSON = response.result.value as? [String: Any] {
+                        //print(JSON)
+                        //take URLs from the json into an ImagesURLsArray
+                        let value = JSON["value"] as? [[String: AnyObject]]
+                        //for imageValue in value! {
+                        if let imageValue = value {
+                            if imageValue.count > 0 {
+                                self.imageURL = URL(string: (imageValue[0]["contentUrl"] as? String)!)
+                                /*Alamofire.download((imageValue["contentUrl"] as? String)!)
+                                 .responseData { response in
+                                 if let data = response.result.value {
+                                 let image = UIImage(data: data)
+                                 }
+                                 }*/
+                                //}
+                            }
+                        }
+                    }  else {
+                        print("error with response.result.value")}
+                case .failure(_):
+                    if let errorNum = response.response?.statusCode {
+                        let stringErrorNum = "{\"error\": \(errorNum)}"
+                        print(stringErrorNum)
+                    }
+                }
+            }
+        }
+    }
     
+    var imageURL: URL? {
+        didSet {
+            fetchImage()
+        }
+    }
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+                print(image)
+                //self.performSegue(withIdentifier: "fromARtoImage", sender: self)
+            }
+        }
+    }
+    
+    private var image: UIImage?
+    
+    
+    @objc
+    func handleTap(gestureReconize: UITapGestureRecognizer) {
+        guard sceneView.session.currentFrame != nil else {
+            return
+        }
+        //guard let pointOfView = sceneView.pointOfView else { return }
+        
+        let image = sceneView.snapshot()
+        if let tessract = G8Tesseract(language: "eng") {
+            tessract.delegate = self
+            tessract.image = image
+            tessract.recognize()
+            print("Recognized text: \(tessract.recognizedText)")
+            dishName = tessract.recognizedText.trim()
+        }
+        //let imagePlane = SCNPlane(width: sceneView.bounds.width / 6000, height: sceneView.bounds.height / 6000)
+        //imagePlane.firstMaterial?.
+        //imagePlane.
+        
+    }
+    
+    func progressImageRecognition(for tesseract: G8Tesseract!) {
+        print("Reconigtion progress: \(tesseract.progress) %")
+    }
+
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
         
@@ -83,3 +180,24 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
     }
 }
+
+extension String {
+    func trim() -> String {
+        return self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+    }
+}
+
+extension UIImage {
+    func crop( rect: CGRect) -> UIImage {
+        var rect = rect
+        rect.origin.x*=self.scale
+        rect.origin.y*=self.scale
+        rect.size.width*=self.scale
+        rect.size.height*=self.scale
+        
+        let imageRef = self.cgImage!.cropping(to: rect)
+        let image = UIImage(cgImage: imageRef!, scale: self.scale, orientation: self.imageOrientation)
+        return image
+    }
+}
+
